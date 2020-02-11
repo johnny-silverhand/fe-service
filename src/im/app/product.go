1de4ff2482c43c2288f1c38a588aa1ee1a6a2307
Дмитрий Николаев <d.nikolaev@russianit.ru>
2020-02-11 11:04:26 +0400

    feat: product private_rule update

--- src/im/app/product.go
+++ src/im/app/product.go
@@ -315,6 +315,7 @@ func (a *App) UpdateProduct(product *model.Product, safeUpdate bool) (*model.Pro
 	newProduct.Measure = product.Measure
 	newProduct.AppId = product.AppId
 	newProduct.Extra = product.Extra
+	newProduct.PrivateRule = product.PrivateRule
 
 	newProduct.Status = model.PRODUCT_STATUS_DRAFT
 
