131ee437778809dad8981ed0e8a66b10b35972f2
Johnny Silverhand <johnny-silverhand@outlook.com>
2020-03-19 12:59:35 +0400

    fix

--- src/im/store/sqlstore/user_store.go
+++ src/im/store/sqlstore/user_store.go
@@ -1935,9 +1935,9 @@ func (us SqlUserStore) GetMetricsForBonuses(options model.UserGetOptions) store.
 		}
 
 		if options.Invited {
-			query = query.Where("LENGTH(u.InvidedBy) = ? ", 26)
+			query = query.Where("LENGTH(u.InvitedBy) = ? ", 26)
 		} else {
-			query = query.Where("LENGTH(u.InvidedBy) > ? ", 0)
+			query = query.Where("LENGTH(u.InvitedBy) > ? ", 0)
 		}
 
 		queryString, args, err := query.ToSql()
